879b82f68dd3b11cf4a87b952e1a9b2770973214
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-04-18 13:51:08 +0530

    Time: 204 ms (40.16%), Space: 51 MB (6.31%) - LeetHub

---
+++ 87-scramble-string/87-scramble-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public: 
+    map<string ,int>m;
+     int findans(string a,string  b )
+    {  
+         if(a.size()!=b.size())
+             return 0;
+         if(a.compare(b)==0)
+             return 1;  
+         map<char,int>mm;
+         for(auto i:a)
+             mm[i]++;
+         for(auto i:b)
+             mm[i]--;
+         for(auto i:mm)
+             if(i.second>0)
+                 return 0;
+         if(a.size()<=1)
+             return 0;
+            string key=a;
+         key+=" ";
+         key.append(b);
+         if(m.find(key)!=m.end())
+             return m[key];
+         int n=a.size();
+         int flag=0;
+         for(int k=0;k<n-1;k++)
+         {  
+             int ans=0;
+                        // if not swapped 
+                int first=findans(a.substr(0,k+1),b.substr(0,k+1)) &&findans(a.substr(k+1,n ),b.substr(k+1,n )); 
+                // if swapped 
+            int second=(findans(a.substr(0,k+1),b.substr(n-1-k,(k+1)))) && findans(a.substr(k+1,n ),b.substr(0,n-(k+1))); 
+            ans=first|second;
+            if(ans==1)
+                flag=1;
+          
+         }
+         return m[key]=flag;
+        
+         
+    }
+    bool isScramble(string s1, string s2) { 
+        return findans(s1,s2 );
+    }
+};
