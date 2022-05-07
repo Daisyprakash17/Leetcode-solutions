75fd3602cb25f258a8304007494bedb3a4012ca1
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-05-07 19:33:14 +0530

    Time: 217 ms (11.17%), Space: 55.5 MB (9.05%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) { 
+        int n=nums.size();
+        vector<int>v(n);
+        v[0]=nums[0];
+        set<int>s; 
+        for(int i=1;i<n;i++)
+        {
+            
+         v[i]=min(nums[i],v[i-1]); 
+        }
+        s.insert(nums[n-1]);
+        for(int i=n-2;i>0;i--)
+        { 
+            
+            int mi=v[i-1];
+            if(nums[i]>mi)
+            {
+                auto it=s.upper_bound(mi);
+                if(it!=s.end() && *it<nums[i])
+                    return true;
+            }
+            s.insert(nums[i]);
+        }
+        return false;
+        
+        
+    }
+}; 
