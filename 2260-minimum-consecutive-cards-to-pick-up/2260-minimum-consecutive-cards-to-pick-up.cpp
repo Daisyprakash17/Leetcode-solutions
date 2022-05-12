4dbc229ba9213b3dba07a58f175ef4233a2ddf8b
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-05-12 19:08:18 +0530

    Time: 524 ms (55.26%), Space: 120.2 MB (33.77%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int minimumCardPickup(vector<int>& v) {
-        map<int,vector<int>>m;
+        map<int,int>m;
+        
+            int ans=INT_MAX;
         for(int i=0;i<v.size();i++)
         {
-                m[v[i]].push_back(i+1);
+            int val=v[i];
+            if(m[val]!=0) 
+                ans=min(ans,(i+1)-m[val]);
+                m[val]=i+1;
         }
-            int ans=INT_MAX;
-        for(auto i:m)
-        {
-            vector<int>v=i.second;
-            if(v.size()>1)
-            {
-                for(int j=1;j<v.size();j++)
-                {
-                    ans=min(ans,v[j]-v[j-1]);
-                }
-            }
-        }
+        
         if(ans==INT_MAX)
             return -1;
         else
