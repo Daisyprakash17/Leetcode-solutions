3fff29062333d9369d91c0fd3f38867e19a59915
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-05-15 11:21:12 +0530

    Time: 50 ms (14.62%), Space: 14.3 MB (43.95%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -2,42 +2,41 @@ class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& v) {
         
-        
-        vector<int>arr[n]; 
+        vector<int>vis(n,0);
+        vector<int>arr[n];
+        for(auto i:v)
+            arr[i[1]].push_back(i[0]);
+        int counter=0;
         vector<int>in(n,0);
-        for(int i=0;i<v.size();i++)
-        {
-            arr[v[i][1]].push_back(v[i][0]); 
-        }
-        
         for(auto i:arr)
-        {
-            
             for(auto j:i)
                 in[j]++;
-        }
         queue<int>q;
         for(int i=0;i<n;i++)
-        {
             if(in[i]==0)
-                q.push(i);
-        } 
+            {
+                
+             q.push(i);
+            cout<<i<<endl;
+            }
+        // we will push all the nodes whose in degree is zero in the queue
         
-        int count=0;
         while(q.size()>0)
         {
-            auto it=q.front();
+            int node=q.front();
             q.pop();
-            count++;
-            for(auto i:arr[it])
+            counter++;
+            for(auto i:arr[node])
             {
                 in[i]--;
                 if(in[i]==0)
                     q.push(i);
             }
         }
-        return (n==count);
-        
+        if(counter==n)
+            return true;
+        else
+            return false;
         
     }
 };
