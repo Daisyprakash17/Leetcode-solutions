fcf9c2134658e8fdd9aeb1003275d4f3de4013f6
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-05-29 23:49:03 +0530

    Time: 61 ms (5.97%), Space: 18 MB (90.21%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
     
-    bool findans(vector<vector<char>>&v,int r,int c,int n)
+    bool findans(vector<vector<char>>&v,int r,int c)
     {
         
-        int i=3*(r/3);
-        int j=3*(c/3);
-         map<int,char>m;
-        for(int k=i;k<i+3;k++)
-        {
-            for(int l=j;l<j+3;l++)
-            {
-                if(v[k][l]!='.')
-                m[v[k][l]]++;
-            }
-        }
-         for(auto i:m)
-            if(i.second>1)
-                return false;
+          char val=v[r][c];
+         v[r][c]='.';
+         int flag=1;
+         
+         for(int i=0;i<9;i++)
+         {
+             if(v[i][c]==val || v[r][i]==val)
+             {
+                 flag=0;
+                 break;
+             }
+             
+             if(v[3*(r/3)+i/3][3*(c/3)+i%3]==val)
+             {
+                 flag=0;
+                 break;
+             }
+                 
+         }
+         
+         v[r][c]=val;
+         return flag;
         
-         for(int j=0;j<n;j++)
-        {
-            if(j!=r && v[j][c]==v[r][c])
-                return false;
-        }
-        
-         for(int j=0;j<n;j++)
-        {
-            if(j!=c && v[r][j]==v[r][c])
-                return false;
-        }
-        
-         return true;
         
         
     }
@@ -43,7 +38,7 @@ public:
         {
             for(int j=0;j<n;j++)
             {
-                if(v[i][j]!='.' && !findans(v,i,j,n))
+                if(v[i][j]!='.' && !findans(v,i,j))
                 {
                    
                   return false;
