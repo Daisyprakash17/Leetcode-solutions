f213d91a0c930b0446bee252e63e9b89eddf6b11
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-03 16:51:17 +0530

    Time: 179 ms (31.57%), Space: 6.1 MB (56.67%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,35 +1,29 @@
 class Solution {
-public:
-    vector<int>vis;
-    vector<int>temp;
-    int ans=0;
-    void findans( )
+public: 
+    void findans(vector<int>&vis,int &ans,int count)
     {
         
-        if(temp.size()==vis.size())
-        {
-            
-                ans++; 
+        if(count==vis.size())
+        { 
+            ans++; 
             return ;
         }
         for(int i=0;i<vis.size();i++)
         {
-            if(vis[i]==0 && ((i+1)%(temp.size()+1)==0 || (temp.size()+1)%(i+1)==0))
+            if(vis[i]==0 && ((i+1)%(count+1)==0 || (count+1)%(i+1)==0))
             {
-                vis[i]=1;
-                temp.push_back(i+1);
-                findans();
-                vis[i]=0;
-                temp.pop_back();
+                vis[i]=1; 
+                findans(vis,ans,count+1);
+                vis[i]=0; 
             }
         }
     }
     int countArrangement(int n) {
        
-        vector<int>a(n,0); // 0 denote that element is available
-        
-        vis=a;
-        findans();
+        vector<int>vis(n,0); // 0 denote that element is available
+         int ans=0;
+        int count=0;
+        findans(vis,ans,count);
         return ans;
     }
 };
