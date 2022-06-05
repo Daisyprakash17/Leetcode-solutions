813b73551df97f3d4d19e6c540f023d1d9dba560
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-05 17:46:08 +0530

    Time: 89 ms (32.64%), Space: 17.5 MB (28.71%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -3,31 +3,45 @@ public:
     int maxPoints(vector<vector<int>>& p) {
         
         int ans=0;
+        // sort all the value to make sure x2-x1 of the slope will all >=0
         sort(p.begin(),p.end());
         int n=p.size();
         for(int i=0;i<n;i++)
         {
-            
+            // for each points calculating the slope and storing the
+            // in form of pair {numrator,denomrator}
             map<pair<int,int>,int>m; 
             for(int j=0;j<n;j++)
             {
-                if(i==j)
+                if(i==j)  // continue the loop for same point
                     continue;
-                int first=p[j][1]-p[i][1];
-                int second=p[j][0]-p[i][0];
-                int hcf=__gcd(first,second); 
+                
+                
+                // calculating the numrator and denomator
+                int num=p[j][1]-p[i][1];
+                int deno=p[j][0]-p[i][0];
+                
+                // taking the hcf of the value and dividing each with hcf so as to reduce 
+                int hcf=__gcd(deno,num); 
+                
+                // if hcf is one makign hcf as 1 so as to deal with the edges cases
                 if(hcf==0)
                     hcf=1;
-                first/=hcf;
-                second/=hcf; 
-                m[{first,second}]++;
+                deno/=hcf;
+                num/=hcf; 
+                
+                // increamenting the slope which is stored in for of pair {numm,demo}
+                m[{num,deno}]++;
                 
             }
+            
+            // checking the map for the maximun repetaive slope 
             for(auto i:m)
                 ans=max(ans,i.second);
                 
             
         }
+        // return ans+1 so as to count the point itself 
         return ans+1;
     }
 };
