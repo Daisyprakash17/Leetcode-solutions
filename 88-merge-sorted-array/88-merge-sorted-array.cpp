bb8ff89f37155686d9140086f063609ede8ac736
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-07 10:24:18 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (99.28%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,35 +1,9 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int n, vector<int>& nums2, int m) {
-         int i=n-1;
-        int j=m-1;
-        int k=nums1.size()-1;
-        //  scanning both the array from the back 
-        // and also keeping the pointer the last locationg of 
-        // first array and pushing the maximum value out of the 
-        // array and decreamenting the last location index
-        while(j>=0 && i>=0)
-        {
-            if(nums1[i]>=nums2[j])
-            {
-                nums1[k]=nums1[i];
-                i--;
-                k--;
-            }
-            else
-            {
-                nums1[k]=nums2[j];
-                k--;
-                j--;
-            }
-        } 
-        
-        // running the loop if j>=0 so push all the value from this
-        while(j>=0)
-        {
-            nums1[k]=nums2[j];
-                k--;
-                j--;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i=m;i<m+n;i++){
+            nums1[i]=nums2[i-m];
         }
+        sort(nums1.begin(),nums1.end()); 
     }
 };
