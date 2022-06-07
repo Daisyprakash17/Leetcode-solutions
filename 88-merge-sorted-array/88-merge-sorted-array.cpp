03cede16a223f3e473e2498fddea722d5d828a55
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-07 10:15:45 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,16 +4,32 @@ public:
          int i=n-1;
         int j=m-1;
         int k=nums1.size()-1;
-        while(j>=0 && i>=0) 
-            if(nums1[i]>=nums2[j]) 
-                nums1[k--]=nums1[i--];  
-            else 
-                nums1[k--]=nums2[j--];   
-        // if i is >=0 then run this loop
-        while(i>=0) 
-             nums1[k--]=nums1[i--];  
+        //  scanning both the array from the back 
+        // and also keeping the pointer the last locationg of 
+        // first array and pushing the maximum value out of the 
+        // array and decreamenting the last location index
+        while(j>=0 && i>=0)
+        {
+            if(nums1[i]>=nums2[j])
+            {
+                nums1[k]=nums1[i];
+                i--;
+                k--;
+            }
+            else
+            {
+                nums1[k]=nums2[j];
+                k--;
+                j--;
+            }
+        } 
         
-        while(j>=0) 
-            nums1[k--]=nums2[j--];  
+        // running the loop if j>=0 so push all the value from this
+        while(j>=0)
+        {
+            nums1[k]=nums2[j];
+                k--;
+                j--;
+        }
     }
 };
