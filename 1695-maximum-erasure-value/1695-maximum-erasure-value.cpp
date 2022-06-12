3b9dd811ffeeb098db030397ee23d4e4cc24add5
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-12 10:59:16 +0530

    Time: 663 ms (25.89%), Space: 101.6 MB (67.80%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    int maximumUniqueSubarray(vector<int>& s) {
-         long long  ans=-1;
-        int count=0; 
-        vector<long long >pre;
-        pre.push_back(0); 
-        
-        // her e
-        for(int i=1;i<=s.size();i++)
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int sum=0;
+        int i=0; 
+        int ans=0;
+        map<int,int>m;
+        for(int j=0;j<nums.size();j++)
         {
-            long long cur=s[i-1];
-            pre.push_back(cur+pre[i-1]);
-        }
-        // last index where problem arises
-        int last=0; 
-        map<int,int>m;  
-        set<pair<int,int>>ss;
-        for(int i=0;i<s.size();i++)
-        {
-        
-            if(m[s[i]]!=0 && m[s[i]]>last)
+            sum+=nums[j];
+            m[nums[j]]++;
+            
+            while(m[nums[j]]!=1)
             {
-                // so i will do something
-                 
+                m[nums[i]]--;
+                sum-=nums[i];
+                i++;
                 
-                last=m[s[i]]; 
-                auto it=ss.begin();
-                while(it!=ss.end()  && (*it).first<=last)
-                {
-                    m.erase((*it).second);
-                    ss.erase(it);
-                    it=ss.begin();
-                }
             }
-            m[s[i]]=i+1;
-            ss.insert({i+1,s[i]});
-            ans=max(ans,pre[i+1]-pre[last]);
+            ans=max(ans,sum);
         }
         return ans;
     }
