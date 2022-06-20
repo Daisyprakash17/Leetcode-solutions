83522e4dda0b161fa54a187e28b6622ff70ce044
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-20 09:29:26 +0530

    Time: 645 ms (5.45%), Space: 40.7 MB (48.52%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& v) {
+        map<string ,int >m;
+        vector<pair<int,string>>vv;
+        
+        for(auto i:v)
+        {
+            m[i]++;
+            vv.push_back({i.size(),i});
+        }
+        sort(vv.rbegin(),vv.rend());
+        
+        int count=0;
+        for(auto i:vv)
+        { 
+            if(m[i.second]>0)
+            {
+                cout<<"checking for "<<i.second<<endl;
+                count+=i.second.size()+1;
+                m[i.second]=0;
+                for(int j=0;j<i.second.size();j++)
+                {
+                    string temp=i.second.substr(j,7);
+                    cout<<"temp is "<<temp<<endl;
+                    if(m[temp]>0)
+                    {
+                        m[temp]=0;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
