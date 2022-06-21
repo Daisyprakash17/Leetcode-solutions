c708506f2d9b8670d86d58eb1f8650acab24cd17
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-21 23:15:59 +0530

    Time: 4 ms (33.72%), Space: 7.9 MB (6.15%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    int findans(vector<int>&v,vector<int>&dp,int n)
+    {
+        cout<<n<<endl;
+        if(n<=0)
+            return 0;
+        if(dp[n-1]!=-1)
+            return dp[n-1];
+        
+        return dp[n-1]=max(v[n-1]+findans(v,dp,n-2),findans(v,dp,n-1));
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        
+        vector<int>dp(n,-1);
+        
+        return findans(nums,dp,n);
+        
+    }
+};
