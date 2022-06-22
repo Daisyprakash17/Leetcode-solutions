8ce67f687220463544e0b67367412d6ead98b35c
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-22 20:14:55 +0530

    Time: 32 ms (27.25%), Space: 20.9 MB (7.15%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     map<int,int>m;
-    int findans(vector<int>&dp,int n)
+    int findans(vector<int>&dp,int n,vector<int>&val)
     {
         if(n<=0)
-            return 0;
+            return 0; 
         if(dp[n]!=-1)
             return dp[n];
         
-        return dp[n]=max(m[n]*n+findans(dp,n-2),findans(dp,n-1));
+        int ans=0;
+        if(m[val[n-1]-1]>0)
+            ans=max(val[n-1]*m[val[n-1]]+findans(dp,n-2,val),findans(dp,n-1,val));
+        else
+        {
+            ans=val[n-1]*m[val[n-1]]+findans(dp,n-1,val);
+        }
+        return dp[n]=ans;
     }
     int deleteAndEarn(vector<int>& nums) {
         
         for(auto i:nums)
             m[i]++;
-    vector<int>dp(10001,-1);
-        sort(nums.begin(),nums.end());
-        return findans(dp,10000);
+        vector<int>val;
+        for(auto i:m)
+            val.push_back(i.first);
+        int n=val.size();
+        vector<int>dp(n+1,-1);
+        return findans(dp,n,val);
     }
 };
