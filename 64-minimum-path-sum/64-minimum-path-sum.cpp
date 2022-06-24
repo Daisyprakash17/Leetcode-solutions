de475cee2d301ed84288224340ffcbefb121cd04
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-25 00:44:44 +0530

    Time: 100 ms (5.76%), Space: 10 MB (59.47%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& v) {
+        
+        int n=v.size();
+        int m=v[0].size();
+        vector<vector<int>>temp(n,vector<int>(m,INT_MAX));
+        temp[n-1][m-1]=v[n-1][m-1];
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                cout<<i<<" "<<j<<endl;
+                if(i+1<n && temp[i][j]>temp[i+1][j]+v[i][j])
+                    temp[i][j]=temp[i+1][j]+v[i][j];
+                    
+                if(j+1<m && temp[i][j]>temp[i][j+1]+v[i][j])
+                    temp[i][j]=temp[i][j+1]+v[i][j];
+            }
+        }
+        
+        return temp[0][0];
+    }
+};
