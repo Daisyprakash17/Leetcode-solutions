d47561d7119aad2961af378348bdbcd30c6a862d
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-25 15:31:05 +0530

    Time: 472 ms (10.68%), Space: 10.8 MB (58.92%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int dp[502][502];
-    int findans(string &a,string &b ,int n,int m)
-    {
-        if(n==0 || m==0)
-            return 0;
-        if(dp[n][m]!=-1)
-            return dp[n][m];
-        if(a[n-1]==b[m-1])
-            return dp[n][m]=1+findans(a,b,n-1,m-1);
-        else
-            return dp[n][m]=max(findans(a,b,n-1,m),findans(a,b,n,m-1));
-    }
+    	int dp[1001][1001];
+	   int findans(string &a,string &b,int n,int m)
+	   {
+	       if(n==0 || m==0)
+	       return 0;
+	       if(dp[n][m]!=-1)
+	            return dp[n][m];
+	           
+	       if(a[n-1]==b[m-1])
+	       return dp[n][m]=1+findans(a,b,n-1,m-1);
+	       else
+	       return dp[n][m]=max(findans(a,b,n,m-1),findans(a,b,n-1,m));
+	   }
     int minDistance(string a, string b) {
-    
-        memset(dp,-1,sizeof(dp));
-        int val=findans(a,b,a.size(),b.size());
-         return a.size()-val+(b.size()-val);
+        int n=a.size();
+	    int m=b.size();
+	    memset(dp,-1,sizeof(dp));
+	    int val=findans(a,b,n,m);
+        return n+m-2*val;
     }
 };
