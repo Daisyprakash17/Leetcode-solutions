2dee29c5e4b2aaaea775a7d86644fbec3fc7cb76
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-25 13:04:18 +0530

    Time: 31 ms (57.34%), Space: 15 MB (10.33%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -17,8 +17,28 @@ public:
     }
     int longestCommonSubsequence(string a, string b) {
         int n=a.size();
-        int m=b.size(); 
-        memset(dp,-1,sizeof(dp));
-        return findans( a,b,n,m);
+        int m=b.size();  
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+            
+            
+        }
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++) 
+                if(a[i-1]==b[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+            else
+                dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+            
+            
+        }
+        return dp[n][m];
+        
     }
 };
