7dd200d434e2700047bd4dbdd30d3e24e3bebb14
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-25 15:20:35 +0530

    Time: 35 ms (40.03%), Space: 10.7 MB (66.17%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
+    int dp[1001][1001];
     
-   int dp[1001][1001];
-    void findans(string a,string b,int n,int m)
+    void findans(string &a,string &b,int n,int m)
     {
-        for(int i=1;i<=n;i++)
+          for(int i=1;i<=n;i++)
         dp[i][0]=i;
         for(int j=1;j<=m;j++)
         dp[0][j]=j;
@@ -25,48 +25,52 @@ public:
     }
     string shortestCommonSupersequence(string a, string b) {
         
+        int n=a.size();
+        int m=b.size();
         memset(dp,-1,sizeof(dp));
-        findans(a,b,a.size(),b.size());
         
-        
-        string ans;
-        int i=a.size();
-        int j=b.size();
+      
+        findans(a,b,n,m);
+         
+        string s;
+        int i=n;
+        int j=m;
         while(i>0 && j>0)
         {
-             if(a[i-1]==b[j-1])
+            
+            if(a[i-1]==b[j-1])
             {
-                ans+=a[i-1];
-                 i--;
+                s+=a[i-1];
+                i--;
                 j--;
-                
             }
             else
             {
-                // if not equal then 
-                if(dp[i][j-1]<=dp[i-1][j])
-                {
-                    ans+=b[j-1];
-                     j--;
+                if(dp[i][j-1]<=dp[i-1][j]){
+                    s+=b[j-1];
+                    j--;
                 }
                 else
                 {
-                    ans+=a[i-1];
-                     i--;
+                    s+=a[i-1];
+                    i--;
+                    
                 }
             }
         }
-          while(i>0)
+        while(i>0)
         {
-            ans+=a[i-1];
-            i--;
+            s+=a[i-1];
+                    i--;
         }
         while(j>0)
         {
-            ans+=b[j-1];
-            j--;
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+                    s+=b[j-1];
+                    j--;
+    }
+        reverse(s.begin(),s.end());
+        return s;
+    
+        
     }
 };
