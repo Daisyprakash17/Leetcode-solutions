e114e1bdc87008fbdf42c6028e16a5849eb315dd
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-26 12:55:41 +0530

    Time: 76 ms (57.87%), Space: 27.9 MB (17.64%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,32 +1,46 @@
-
 class Solution {
-private:
-    bool getMatch(int i, int j, string &s, string &p, vector<vector<int>>& dp){
-        if(j<=0 && i<=0) return true;
-        if(j<=0) return false;
-        if(i<=0){
-            while(j > 0)
+public: 
+    
+    bool findans(string &a,string &p,int n,int m,vector<vector<int>>& dp)
+    {
+        if(m==0 )
             {
-                if(p[j-1]!='*')
+                if(n!=0)
                     return false;
-                j--;
+                else
+                    return true;
+        }
+        if( n==0)
+        {
+            while(m--)
+            {
+                if(p[m]!='*')
+                    return false;
             }
             return true;
         }
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(i>0 && (s[i-1]==p[j-1] || p[j-1]=='?'))
-            return dp[i][j] = getMatch(i-1, j-1, s, p, dp);
-        else if(p[j-1]=='*') 
-            return dp[i][j] = getMatch(i-1, j, s, p, dp) || getMatch(i, j-1, s, p, dp);
-        else 
-            return dp[i][j] = false;
+        
+        
+        if(dp[n][m]!=-1)
+            return dp[n][m];
+        
+        if(a[n-1]==p[m-1] || p[m-1]=='?' )
+            return dp[n][m]=findans(a,p,n-1,m-1,dp);
+        else
+        {
+            if(p[m-1]=='*')
+                return dp[n][m]=findans(a,p,n-1,m,dp) || findans(a,p,n,m-1,dp);
+            else
+                return false;
+        }
+        
     }
-public:
-    bool isMatch(string s, string p) {
-        int n = s.size(), m = p.size(); 
+    bool isMatch(string a, string p) {
+        
+      
+        int n = a.size(), m = p.size(); 
        
         vector<vector<int>>dp (n+1, vector<int>(m+1, -1));
-        return getMatch(n, m, s, p, dp);
+        return findans(a,p,a.size(),p.size(),dp);
     }
 };
