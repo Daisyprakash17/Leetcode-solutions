ad565cf83c68b3ccc4ebc99d54bf939fb2848ff5
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-06-27 12:14:55 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -11,60 +11,67 @@ class Solution{
 public:
     int dp[201][201][2];
     int findans(string &s,int i,int j,int val)
-    { 
-         if(i>j)
+    {
+        if(i>j)// return false as this is null sting 
         return 0;
         if(i==j)
-        {
+            {
+                if(val==1)
+                return s[i]=='T';
+                else
+                return s[i]=='F';
+            }
             
-            if((s[i]=='T' && val==1) ||(s[i]=='F' && val == 0))
-            return 1;
-            else
-            return 0;
-        }
         if(dp[i][j][val]!=-1)
         return dp[i][j][val];
+        
         int count=0;
         for(int k=i+1;k<j;k+=2)
         {
             int lt=findans(s,i,k-1,1);
             int lf=findans(s,i,k-1,0);
             int rt=findans(s,k+1,j,1);
-            int rf=findans(s,k+1,j,0);  
-              if(s[k]=='&')
+            int rf=findans(s,k+1,j,0);
+            
+            if(s[k]=='&')
+            {
+                if(val==1)
+                count+=lt*rt;
+                else
+                count+=lt*rf+lf*rt+rf*lf;
+                
+                count=(count)%1003;
+            }
+            else if(s[k]=='^')
+            {
+                if(val==1)
                 {
-                    if(val==1)
-                    count+=lt*rt;
-                    else 
-                    count+=lt*rf+lf*rt+lf*rf;
-                    count=count%1003;
+                    count+=lt*rf+lf*rt;
                 }
-                else if(s[k]=='|')
+                else
                 {
-                    if(val==1)
-                    {
-                        count+=rt*lt+lt*rf+lf*rt;
-                        
-                    }
-                    else
-                    count+=lf*rf;
-                    count=count%1003;
+                    count+=lt*rt+lf*rf;
                 }
-                else if(s[k]=='^')
-                {
-                    if(val==1)
-                    count+=lf*rt+lt*rf;
-                    else
-                    count+=lf*rf+lt*rt;
-                    count=count%1003;
-                }
+                
+                count=(count)%1003;
+            }
+            else
+            {
+                if(val==1)
+                count+=lf*rt+rf*lt+lt*rt;
+                else
+                count+=lf*rf;
+                
+                count=(count)%1003;
+            }
         }
-         return dp[i][j][val]=count;
+        return dp[i][j][val]=count;
     }
+    
     int countWays(int n, string s){
         // code here
         memset(dp,-1,sizeof(dp));
-     return findans(s,0,n-1,1); 
+        return findans(s,0,n-1,1); // we are calling this function as we want positive values
     }
 };
 
