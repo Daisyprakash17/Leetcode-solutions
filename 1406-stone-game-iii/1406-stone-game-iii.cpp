d244cea7b882d634908d446b706c4dcf067eabd9
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-08-08 15:37:09 +0530

    Time: 284 ms (87.69%), Space: 133.7 MB (56.15%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -4,31 +4,29 @@ public:
     int findans(vector<int>&v,int i,int who)
     {
             if(i>=v.size())
-            {
-                return 0; 
-            }
+                return 0;
         if(dp[i][who]!=-1)
             return dp[i][who];
         int ans=INT_MIN;
         if(who==0)
             ans=INT_MAX;
+        
+        int sum=0; 
         int count=0;
-        int sum=0;
         for(int j=i;j<v.size() && count<3;j++)
         {
             count++;
             sum+=v[j];
             if(who==1)
-            ans=max(ans,sum+findans(v,j+1,0));
+                ans=max(ans,sum+findans(v,j+1,0));
             else
-            ans=min(ans,-sum+findans(v,j+1,1));
-        } 
+                ans=min(ans,-sum+findans(v,j+1,1));
+        }
         return dp[i][who]=ans;
-        
     }
     string stoneGameIII(vector<int>& stoneValue) {
-        memset(dp,-1,sizeof(dp));
-         int val= findans(stoneValue,0,1); 
+    memset(dp,-1,sizeof(dp));
+        int val=findans(stoneValue,0,1);
         if(val>0)
             return "Alice";
         else if(val<0)
