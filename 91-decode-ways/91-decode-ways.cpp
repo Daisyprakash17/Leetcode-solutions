2782752cfd395acfa3d6de972a72803dcc530045
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-10-02 10:07:19 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (93.12%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int dp[101];
-    int findans(string s,int n)
+    int findans(string &s,int n,int i)
     {
-        if(n<=0)
+        if(i==n)
             return 1;
         
-        if(dp[n]!=-1)
-            return dp[n];
-        int sum=0;
-        if(s[n-1]!='0')
+        if(s[i]=='0')
+            return 0;
+        if(dp[i]!=-1)
+            return dp[i];
+         int val=0;
+        // this for single digit 
+            val+=findans(s,n,i+1);
+        
+        // now consider the double digit
+         
+        if(i+1<n)
         {
-            sum+=findans(s,n-1);
+            if(s[i]=='1')
+                val+=findans(s,n,i+2);
+            else if(s[i]=='2' && (s[i+1]>='0' && s[i+1]<='6'))
+                val+=findans(s,n,i+2);
         }
-        if(n-1>0  )
-        {
-            if(s[n-2]=='1')
-            sum+=findans(s,n-2);
-            else if(s[n-2]=='2' && s[n-1]>='0' && s[n-1]<='6')
-                sum+=findans(s,n-2);
-        } 
-        return dp[n]=sum;
+        
+        return dp[i]=val;
     }
     int numDecodings(string s) {
+        int n=s.size();
         memset(dp,-1,sizeof(dp));
-        return findans(s,s.size());
+        
+        return findans(s,n,0);
     }
 };
