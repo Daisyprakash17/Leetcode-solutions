8f7943693cfce2e4f88e01b29ffaf1b50972624a
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-10-02 10:14:42 +0530

    Time: 5 ms (15.21%), Space: 5.9 MB (70.14%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int numTrees(int n) {
-        vector<int>v(20,0);
+        vector<int>v(25,0);
         v[0]=1;
         v[1]=1;
         
         for(int i=2;i<=n;i++)
-        {
-            for(int j=i-1;j>=0;j--)
+        { 
+            for(int j=0;j<i;j++)
             {
-                int left=j;
-                int right=i-1-j;
-                v[i]+=v[left]*v[right];
-            }
+                v[i]+=v[j]*v[(i-1)-j];
+            } 
         }
-            
         return v[n];
     }
 };
