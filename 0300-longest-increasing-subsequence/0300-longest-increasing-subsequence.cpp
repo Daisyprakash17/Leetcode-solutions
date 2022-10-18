83a0f2535afc20bf7b526fb26826b024e020fe1b
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-10-18 18:01:19 +0530

    Time: 13 ms (90.25%), Space: 10.4 MB (89.42%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int dp[2501][2501];
-    int findans(vector<int>&v,int n,int pre)
-    {
-        if(n<0)
-            return 0;
-        if(pre!=-1 && dp[n][pre]!=-1)
-            return dp[n][pre];
-        int ans=0;
-        // if it can be consider then also we have a choice to 
-        // either considering that value or not considering that values
-        if(pre==-1 || v[pre]>v[n])
-            ans=max(ans,max(findans(v,n-1,pre),1+findans(v,n-1,n)));
-        else
-            ans=max(ans,findans(v,n-1,pre));
-        if(pre==-1)
-            return ans;
-        return dp[n][pre]=ans;
-    }
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
+        vector<int>v;
+        v.push_back(nums[0]);
         
-        return findans(nums,nums.size()-1,-1);
+        for(int i=1;i<nums.size();i++)
+        {
+            int j=v.size();
+            if(nums[i]>v[j-1])
+                v.push_back(nums[i]);
+            else
+            {
+                int ind=lower_bound(v.begin(),v.end(),nums[i])-v.begin();
+                if(ind<j)
+                    v[ind]=nums[i];
+            }
+        } 
+        return v.size();
     }
 };
