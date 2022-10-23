1250f04568a004021dd426f44ec9869ef8d56953
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-10-23 16:06:12 +0530

    Time: 2106 ms (13.43%), Space: 274.3 MB (16.16%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,52 +1,30 @@
 class Solution {
 public:
-    int pre[101][101];
-    bool  check(int i,int j,int k,int l,int t)
-    {
-        
-        int sum=pre[k][l];
-        if(i-1>=0 && j-1>=0)
-            sum+=pre[i-1][j-1];
-        if(i-1>=0)
-            sum-=pre[i-1][l];
-        if(j-1>=0)
-            sum-=pre[k][j-1];
-        return sum==t;
-    }
     int numSubmatrixSumTarget(vector<vector<int>>& v, int target) {
         int n=v.size();
         int m=v[0].size();
-        memset(pre,0,sizeof(pre));
+        
         for(int i=0;i<n;i++)
         {
-            pre[i][0]=v[i][0];
             for(int j=1;j<m;j++)
-            {
-                pre[i][j]=pre[i][j-1]+v[i][j];
-            }
-        }
-        
-        for(int j=0;j<m;j++)
-        {
-            pre[0][j]=pre[0][j];
-            for(int i=1;i<n;i++)
-                pre[i][j]+=pre[i-1][j];
+                v[i][j]+=v[i][j-1];
         }
         
         int ans=0;
-        
-     
-        for(int i=0;i<n;i++)
+        for(int start=0;start<m;start++)
         {
-            for(int j=0;j<m;j++)
+            for(int last=start;last<m;last++)
             {
-                for(int k=i;k<n;k++)
+                
+                map<int,int>mm;
+                int sum=0;
+                mm[0]=1;
+                    
+                for(int row=0;row<n;row++)
                 {
-                    for(int l=j;l<m;l++)
-                    {
-                        if(check(i,j,k,l,target))
-                            ans++;
-                    }
+                    sum+=v[row][last]-(start>0?v[row][start-1]:0);
+                    ans+=mm[sum-target];
+                    mm[sum]++;
                 }
             }
         }
