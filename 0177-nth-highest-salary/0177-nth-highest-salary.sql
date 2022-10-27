4ac42c02ae0f6a5642f681c5951215d7e22ab445
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2022-10-27 22:52:44 +0530

    Time: 2129 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -3,7 +3,7 @@ BEGIN
   RETURN (
       # Write your MySQL query statement below.
       select max(salary) from Employee as e1 
-    where N-1 <=(select count(distinct(salary)) from Employee as e2
-                  where e1.salary<e2.salary and e1.id<>e2.id)
+    where N-1 =(select count(distinct(salary)) from Employee as e2
+                  where e1.salary<e2.salary )
   );
 END
