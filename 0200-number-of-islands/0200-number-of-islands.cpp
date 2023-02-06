ca9a3c5ca386b168be102e22e151a111b967120f
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-02-06 16:41:52 +0530

    Time: 34 ms (90.42%), Space: 12.3 MB (87.89%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    void dfs(vector<vector<char>>&v,int i,int j,int n,int m)
+    {
+        if(i<0 || j<0 || i>=n || j>=m || v[i][j]=='0')
+            return ;
+        v[i][j]='0';
+        dfs(v,i+1,j,n,m);
+        dfs(v,i-1,j,n,m);
+        dfs(v,i,j+1,n,m);
+        dfs(v,i,j-1,n,m);
+    }
+    int numIslands(vector<vector<char>>& v) {
+        int count=0;
+        
+        int n=v.size();
+        int m=v[0].size();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(v[i][j]=='1'){
+                    count++;
+                    dfs(v,i,j,n,m);
+                }
+            }
+        }
+        
+        return count;
+    }
+};
