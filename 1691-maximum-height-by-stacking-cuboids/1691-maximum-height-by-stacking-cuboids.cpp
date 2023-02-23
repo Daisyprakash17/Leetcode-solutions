d962605ffb45f43deae1c506a3794402e14ffe24
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-02-23 09:18:38 +0530

    Time: 14 ms (85.99%), Space: 9 MB (99.47%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -1,18 +1,18 @@
  
 class Solution { 
 public:
-    int dp[101][101];
-    int findans(vector<vector<int>>&v,int i,int pre)
+    int dp[101];
+    int findans(vector<vector<int>>&v,int i)
     {
         if(i>=v.size())
             return 0;
-        if(dp[i][pre]!=-1)
-            return dp[i][pre];
+        if(dp[i]!=-1)
+            return dp[i];
         int ans=0;
-        if(v[pre][1]<=v[i][1] && v[pre][2]<=v[i][2])
-            ans=max(ans,v[i][2]+findans(v,i+1,i));
-        ans=max(ans,findans(v,i+1,pre));
-        return dp[i][pre]=ans;
+        for(int j=i+1;j<v.size();j++)
+            if(v[j][1]>=v[i][1] && v[j][2]>=v[i][2])
+                ans=max(ans,v[j][2]+findans(v,j));
+        return dp[i]=ans;
     }
     int maxHeight(vector<vector<int>>& v) {
         memset(dp,-1,sizeof(dp));
@@ -22,7 +22,7 @@ public:
        
         int ans=0;
         for(int i=0;i<v.size();i++)
-        ans=max(ans,v[i][2]+findans(v,i+1,i));
+        ans=max(ans,v[i][2]+findans(v,i));
         return ans;
     }
 };
