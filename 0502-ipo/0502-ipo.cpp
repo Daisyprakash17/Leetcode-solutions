8d818c3ea433b1550efaa13c6c0404c453795307
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-02-23 16:42:54 +0530

    Time: 358 ms (17.69%), Space: 84.3 MB (19.89%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    
+    static bool cmp(pair<int,int>&a,pair<int,int>&b)
+    {
+        if(a.first==b.first)
+            return a.second>b.second;
+        return a.first<b.first;
+    }
     int findMaximizedCapital(int k, int w, vector<int>& p, vector<int>& c)      {
         
         
-        priority_queue<int>q;
+        priority_queue<pair<int,int>>q;
                  vector<pair<int,int>>v;
         for(int i=0;i<p.size();i++) 
             v.push_back({c[i],p[i]}); 
-        sort(v.begin(),v.end());
+        sort(v.begin(),v.end(),cmp);
         int i=0; 
         while(k>0)
         {
@@ -16,15 +21,14 @@ public:
               {
                   if(v[i].first>w)
                       break;
-                  q.push({v[i].second });
+                  q.push({v[i].second,v[i].first});
               }
             if(q.size()==0)
                 break;
-             
-            
-            w+=q.top();
+            auto it=q.top();
             q.pop();
             k--;
+            w+=it.first;
         }
         
         return w;
