1bc3d3323040cfae4a15f9a1ca97eae123d42792
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-03-03 21:01:44 +0530

    Time: 70 ms (23.03%), Space: 25.9 MB (21.21%) - LeetHub

--- 1349-maximum-students-taking-exam/1349-maximum-students-taking-exam.cpp
+++ 1349-maximum-students-taking-exam/1349-maximum-students-taking-exam.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
+    
+     // here we are using the 4D dp first and second for row and col
+     // third to store the stitting arrangement of the pre row 
+    // and fouth is used to maintain the arrangement for the current row
     int dp[8][8][256][256];
     int findans(vector<vector<char>>&v,int i,int j,int pre,int cur)
     {
          if(i==v.size())
         return 0; 
+        
         // new row
         if(j==v[0].size())
           return   findans(v,i+1,0,cur,0);
         
-        
+        // if this state has been already calculated
         if(dp[i][j][pre][cur]!=-1)
             return dp[i][j][pre][cur] ;
         
@@ -18,6 +23,9 @@ public:
         if(v[i][j]=='.'&&!(i-1>=0 && j+1<v[0].size() && v[i-1][j+1]=='@') && !(i-1>=0 && j-1>=0 &&v[i-1][j-1]=='@' ) && !(j-1>=0 && v[i][j-1]=='@'))
         {
             v[i][j]='@';
+            // if it possible to put the student on the current seat of current row
+            // then we will mark this seat in the current row to be occupied by the student and make 
+            // a another recursive call 
           temp=max(temp, 1+ findans(v,i,j+1,pre,cur|(1<<j)));
             v[i][j]='.';
         }
