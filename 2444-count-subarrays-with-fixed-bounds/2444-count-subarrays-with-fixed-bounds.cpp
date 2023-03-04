6cd4bbc8702fe6083d8beff5649578f13fcda6b6
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-03-04 11:07:17 +0530

    Time: 354 ms (5.30%), Space: 78.2 MB (15.10%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,62 +1,39 @@
 class Solution {
 public:
-    long long countSubarrays(vector<int>& nums, int mi, int ma) {
+    long long countSubarrays(vector<int>& v, int minK, int maxK) {
+        long long ans=0;
         
-        vector<int>mini;
-        vector<int>maxi;
-        int n=nums.size();
-        map<int,vector<int>>m;
-        for(int i=0;i<n;i++)
+        vector<int>mi,ma;
+        vector<int>right;
+        for(int i=0;i<v.size();i++)
         {
-            if(nums[i]<mi)
-                mini.push_back(i);
-            
-            if(nums[i]>ma)
-                maxi.push_back(i);
-            
-            if(nums[i]==mi)
-                m[mi].push_back(i);
-            if(nums[i]==ma)
-                m[ma].push_back(i);
+                if(v[i]==minK)
+                    mi.push_back(i);
+                  if(v[i]==maxK)
+                    ma.push_back(i);
+                  if(v[i]<minK  || v[i]>maxK)
+                    right.push_back(i);
         }
-        mini.push_back(n);
-        maxi.push_back(n);
-         
-        long long  ans=0;
         
-        for(int i=0;i<n;i++)
+        right.push_back(v.size());
+        for(int i=0;i<v.size();i++)
         {
-            if(nums[i]>=mi && nums[i]<=ma)
-            {
-                // now i just have to find the bound and then range
-                
-              auto   it=lower_bound(m[mi].begin(),m[mi].end(),i);
-                if(it==m[mi].end())
-                    break;
-                int val=*it;
-                it=lower_bound(m[ma].begin(),m[ma].end(),i);
-                if(it==m[ma].end())
-                    break;
-                val=max(val,*it); 
-                // so now i am having the range 
-                // now i this range is just have to check 
-                // that all values should be great the mi and smaller than ma
-                
-                  it=lower_bound(mini.begin(),mini.end(),i);
-                auto itt=lower_bound(maxi.begin(),maxi.end(),i);
-                if(  ((*it)<i || (*it)>val) &&    (*itt<i ||  *itt>val))
-                {
-                    // then only we can get the answer 
-                     int temp;
-                    auto it=lower_bound(maxi.begin(),maxi.end(),val);
-                     temp=(*it)-val;
-                     it=lower_bound(mini.begin(),mini.end(),val);
-                     temp=min(temp,(*it)-val);
-                    
-                     ans+=temp;
-                 }
-            }
-         }
-        return ans;
+            int mini=-1;
+            int maxi=-1;
+            int ri=-1;
+            auto it=lower_bound(mi.begin(),mi.end(),i);
+            if(it!=mi.end())
+                mini=*it;
+            it=lower_bound(ma.begin(),ma.end(),i);
+            if(it!=ma.end())
+                maxi=*it ;
+            it=lower_bound(right.begin(),right.end(),i) ;
+            ri=*it;
+            if(maxi==-1 || mini==-1)
+                break;
+            if(ri>mini && ri>maxi)
+                ans+=ri-max(mini,maxi);
+        }
+        return ans ;
     }
 };
