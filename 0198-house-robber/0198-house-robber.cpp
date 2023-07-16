88ca9333f2cb9160a69da87171697f06026c0138
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-07-16 23:56:26 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (94.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int dp[100];
+    int dp[101];
     int findans(int i,int n,vector<int>&v)
     {
             if(i>=n)
@@ -14,7 +14,21 @@ public:
         return dp[i]=ans;
     }
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-    return findans(0,nums.size(),nums);
+        if(nums.size()==1)
+            return nums[0];
+        else if(nums.size()==2)
+            return max(nums[0],nums[1]); 
+        
+        
+        dp[0]=0;
+        dp[1]=nums[0];
+        
+        for(int i=1;i<nums.size();i++)
+        {
+            dp[i+1]=max(nums[i]+dp[i-1],dp[i]);
+            
+        }
+        return dp[nums.size()];
+        
     }
 };
