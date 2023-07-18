fcf373486a9179a31629cac1ad5be800a20a6c15
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-07-18 23:01:39 +0530

    Time: 10 ms (70.20%), Space: 9.6 MB (95.31%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& v) {
+        
+        
         int n=v.size();
         int m=v[0].size();
-        vector<vector<int>>val(n,vector<int>(m,INT_MAX));
-        val[n-1][m-1]=v[n-1][m-1];
-        for(int i=n-1;i>=0;i--)
+        
+        for(int j=m-1;j>=0;j--)
         {
-            for(int j=m-1;j>=0;j--)
+            for(int i=n-1;i>=0;i--)
             {
-                if(i-1>=0 && val[i-1][j]>v[i-1][j]+val[i][j])
-                    val[i-1][j]=v[i-1][j]+val[i][j];
-                
-                if(j-1>=0 && val[i][j-1]>v[i][j-1]+val[i][j])
-                     val[i][j-1]=v[i][j-1]+val[i][j];
+                int val=INT_MAX;
+                if(i+1<n)
+                    val=min(val,v[i+1][j]);
+                if(j+1<m)
+                    val=min(val,v[i][j+1]);
+                if(val==INT_MAX)
+                    val=0;
+                v[i][j]+=val;
+            } 
+
         }
-        } 
-        return val[0][0];
+        return v[0][0];
+
     }
 };
