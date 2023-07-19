d45a50cdb46fe4bfd07b83f57ceb4ea453295a93
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-07-19 22:50:20 +0530

    Time: 23 ms (55.92%), Space: 10.6 MB (58.96%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
-    int dp[20001][21];
-        
-    int findans(vector<int>&v,int sum,int n) 
+    int dp[20001][20];
+    int findans(int i,int n,int sum,vector<int>&v)
     {
-        if(sum==0 && n<0)
-            return true;
-        if(n<0 && sum!=0)
-            return false;
-        
-        if(dp[sum][n]!=-1)
-            return dp[sum][n];
-        
-        if(v[n]<=sum)
+        if(i>=n)
         {
-           return  dp[sum][n]=findans(v,sum-v[n],n-1)+findans(v,sum,n-1);
-            
+            if(sum==0)
+                return 1;
+            else
+                return 0;
         }
         
-        else
-            return dp[sum][n]=findans(v,sum,n-1);
-    }
-        int findTargetSumWays(vector<int>& nums, int t) {
-        t=abs(t);
+        if(dp[sum][i]!=-1)
+            return dp[sum][i];
+        
+        int ans=0;
+        if(sum>=v[i])
+            ans+=findans(i+1,n,sum-v[i],v);
+        ans+=findans(i+1,n,sum,v);
+        return dp[sum][i]=ans;
+            
+            
+        }
+    int findTargetSumWays(vector<int>& nums, int target) {
         int sum=0;
         for(auto i:nums)
             sum+=i;
-        if((sum+t)%2==1 || sum<t)
-            return 0;
-        
-        sum=(sum+t)/2;
-        
-        
+        target=abs(target);
+        if((sum+target)%2!=0 ||sum<target)
+            return 0; // this means that ans is not possible
+        sum=abs((target+sum)/2);
         memset(dp,-1,sizeof(dp));
-            return findans(nums,sum,nums.size()-1);
-    
-        
+        return findans(0,nums.size(),sum,nums);
     }
 };
