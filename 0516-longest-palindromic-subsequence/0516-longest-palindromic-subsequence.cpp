0bcea5f21d0b13f198b1b3c8f487a969f1463de6
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-08-13 15:52:49 +0530

    Time: 58 ms (95.37%), Space: 10.9 MB (71.64%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int findans(string &s,string &t,int i,int j)
+    int dp[1000][1000];
+    int findans(string &a,string &b,int i,int j)
     {
-        if(i<0 | j<0)
+        if(i==a.size() || j==b.size())
             return 0;
         if(dp[i][j]!=-1)
             return dp[i][j];
-        if(s[i]==t[j])
+        
+        int val=0;
+        if(a[i]==b[j])
         {
-            return dp[i][j]=1+findans(s,t,i-1,j-1);
+            val=max(val,1+findans(a,b,i+1,j+1));
         }
         else
         {
-            return dp[i][j]=max(findans(s,t,i-1,j),findans(s,t,i,j-1));
+        val=max(val,findans(a,b,i+1,j));
+        val=max(val,findans(a,b,i,j+1));
         }
+        return dp[i][j]=val;
     }
+    
     int longestPalindromeSubseq(string s) {
         memset(dp,-1,sizeof(dp));
         string t=s;
         reverse(t.begin(),t.end());
-        return findans(s,t,s.size()-1,s.size()-1);
+        return findans(s,t,0,0);
     }
 };
