310c301c6d00d5a62b47b76ca43e23348a8321e9
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-08-25 00:09:39 +0530

    Time: 366 ms (70.70%), Space: 141.1 MB (28.21%) - LeetHub

---
+++ 2440-create-components-with-same-value/2440-create-components-with-same-value.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int findans(vector<vector<int>>&v,vector<int>&vis,int& count,int node,int sum ,vector<int>&nums)
+    {
+        vis[node]=1;
+        int cursum=nums[node];
+        for(auto i:v[node])
+        {
+            if(vis[i]==1)
+                continue;
+            int temp=findans(v,vis,count,i,sum,nums);
+            if(temp!=-1)
+                cursum+=temp;
+            else{
+                vis[node]=0;
+                return -1;
+            }
+        }
+        if(cursum==sum)
+        {
+            count++;
+            cursum=0;
+        }
+        else if(cursum>sum)
+            cursum=-1;
+        vis[node]=0;
+        return cursum;
+    }
+    int componentValue(vector<int>& nums, vector<vector<int>>& edges) {
+        int sum=0;
+        int n=nums.size();
+        for(auto i:nums)
+            sum+=i;
+        vector<vector<int>>v(n);
+        for(auto i:edges)
+        {
+            v[i[0]].push_back(i[1]);
+            v[i[1]].push_back(i[0]);
+        }
+        vector<int>vis(n,0);
+        int ans=0;
+        int count=0;
+        findans(v,vis,count,0,6,nums);
+        cout<<count<<endl;
+        for(int i=1;i<=sqrt(sum);i++)
+        {
+            if(sum%i==0)
+            {
+                
+                cout<<i<<" "<<sum/i<<endl;
+                count=0; 
+                
+                if(findans(v,vis,count,0,i,nums)==0)
+                    ans=max(ans,count-1);
+                cout<<"callled for sum "<<i<<" "<<count<<endl;
+                count=0;
+                if(findans(v,vis,count,0,sum/i,nums)==0)
+                    ans=max(ans,count-1);
+                cout<<"callled for sum "<<sum/i<<" "<<count<<endl;
+            }
+        }
+        return ans;
+        
+    }
+};
