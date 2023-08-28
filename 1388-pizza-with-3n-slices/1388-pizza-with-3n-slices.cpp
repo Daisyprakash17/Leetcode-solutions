7799c8ed3cd1ae65fc19f63ba93fa092abf33b13
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-08-28 21:54:46 +0530

    Time: 713 ms (5.16%), Space: 146.9 MB (8.90%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     map<int,map<int,map<int,int>>>dp;
-    int findans(int i,int left,int right,vector<int>&v)
+    int findans(int i,int left,int right,vector<int>&v,int last)
     {
         if(left+right+1==0)
             return 0;
-        if(i>=v.size()+right)
+        if(i>=last)
         {
             if( (left==0 && right==-2))
                 return 0;
@@ -15,13 +15,13 @@ public:
         if(dp[i][left][right]!=0)
             return dp[i][left][right];
         int ans=0;
-        ans=max(v[i]+findans(i+2,left-1,right-2,v),findans(i+1,left+1,right-1,v));
+        ans=max(v[i]+findans(i+2,left-1,right-2,v,last),findans(i+1,left+1,right-1,v,last));
         return dp[i][left][right]=ans;
     }
     int maxSizeSlices(vector<int>& v) {
         int n=v.size(); 
-        int firsttaken=v[0]+findans(2,0,n-4,v);
-        int firstnottaken=findans(1,1,n-2,v);
+        int firsttaken=v[0]+findans(2,0,n-4,v,v.size()-1);
+        int firstnottaken=findans(1,1,n-2,v,v.size());
         return max(firsttaken,firstnottaken);
     }
 };
