56da018bfbd7addb7d97faed5e95bca401cefcfa
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-08-30 22:29:35 +0530

    Time: 96 ms (85.38%), Space: 74.7 MB (30.00%) - LeetHub

--- 0798-smallest-rotation-with-highest-score/0798-smallest-rotation-with-highest-score.cpp
+++ 0798-smallest-rotation-with-highest-score/0798-smallest-rotation-with-highest-score.cpp
@@ -2,48 +2,35 @@ class Solution {
 public:
     int bestRotation(vector<int>& v) {
         int n=v.size();
-        vector<vector<pair<int,int>>>t(n+1,vector<pair<int,int>>());
+        vector<int>t(n+1,0);
         
         for(int i=0;i<n;i++)
         {
             int in=v[i];
             if(i<in)
             {
-                t[i+1].push_back({1,in});
-                t[i+1+(n-1-in)+1].push_back({-1,in});
+                t[i+1]+=1;;
+                t[i+1+(n-1-in)+1]+=-1;
              }
             else
             {
-                t[0].push_back({1,in});
-                t[i-in+1].push_back({-1,in});
-                 t[i+1].push_back({1,in});
-                t[n].push_back({-1,in});
+                t[0]+=1;
+                t[i-in+1]+=-1;
+                 t[i+1]+=1;
+                t[n]-1;
              }
         }
         int ans=-1;
         int in=0;
-        multiset<int>s; 
+        int count=0;
         for(int i=0;i<n+1;i++)
         { 
-            for(auto j:t[i])
-            {  
-                if(j.first==1)
-                    s.insert(j.second);
-                else
-                {
-                    auto it=s.find(j.second);
-                    s.erase(it);
-                }
-                
-            }  
-            
-            int val=s.size();
-            cout<<s.size()<<endl;
-            if(val>ans)
+            count+=t[i];
+            if(count>ans)
             {
-                ans=val;
+                ans=count;
                 in=i;
-            }      
+            }
         } 
         return in;
             
