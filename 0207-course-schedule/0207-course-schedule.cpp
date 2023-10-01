687d01acca854e2eb2d023cdb926aafb2596d462
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-10-01 15:19:42 +0530

    Time: 20 ms (41.72%), Space: 14.9 MB (22.16%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-       bool  dfs(vector<int>arr[],vector<int>&vis,vector<int>&dvis,int node)
-       {
-           
-           vis[node]=1;
-           dvis[node]=1;
-           for(auto i:arr[node])
-           {
-               if(vis[i]==0)
-               {
-                   if(dfs(arr,vis,dvis,i))
-                       return  true ;// this is the cycle
-               }
-               else if(dvis[i]==1)
-                   return true; // this is the cycle 
-               
-           }
-           dvis[node]=0; // devisti the current traversal
-           return false; // there is no cycle 
-       }
-    bool canFinish(int n , vector<vector<int>>& v) {
-        vector<int >arr[n];
+    bool check(vector<vector<int>>&v,int node,vector<int>&vis,vector<int>&dvis){
         
-        for(auto i:v)
+        vis[node]=1;
+        dvis[node]=1;
+        for(auto i:v[node])
         {
-            arr[i[0]].push_back(i[1]);
+                if(vis[i]==0)
+                {
+                    if(check(v,i,vis,dvis)==true){ 
+                            return true;
+                    }
+                } else if(dvis[i]==1)
+                    return true;
         }
+        dvis[node]=0;
+        return false;
+    }
+    bool canFinish(int n, vector<vector<int>>& p) {
+        vector<vector<int>>v(n);
+        for(auto i:p)
+            v[i[0]].push_back(i[1]);
         
-        vector<int>vis(n,0);
-        vector<int>dvis(n,0);
-         
+        vector<int>vis(n,0),dvis(n,0);
+        
         for(int i=0;i<n;i++)
         {
             if(vis[i]==0)
             {
-               if( dfs(arr,vis,dvis,i))
-                   return false;
+                if(check(v,i,vis,dvis)==true)
+                    return false;
             }
         }
         return true;
