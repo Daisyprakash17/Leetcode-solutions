ccdd87aa57ea9063f8e5a82a69528ba467241ce2
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-10-31 00:14:42 +0530

    Time: 92 ms (72.13%), Space: 76.2 MB (32.50%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    vector<vector<string >>ans;
-    bool check(int i,int j,string &s)
-    {
+    vector<vector<string>>ans;
+    bool check(string &s,int i,int j){
+        
         while(i<=j)
         {
             if(s[i]!=s[j])
@@ -12,28 +12,24 @@ public:
         }
         return true;
     }
-    void findans(int i,string &s,vector<string >&temp)
-    {
+    void findans(string & s,int i,vector<string> &temp){
         if(i>=s.size())
         {
             ans.push_back(temp);
             return ;
         }
-        
-        for(int j=i;j<s.size();j++)
-        {
-            if(check(i,j,s))
-            {
-                string ss=s.substr(i,j-i+1);
-                temp.push_back(ss);
-                findans(j+1,s,temp);
+        for(int j=i;j<s.size();j++){
+            if(check(s,i,j)){
+                temp.push_back(s.substr(i,j-i+1));
+                findans(s,j+1,temp);
                 temp.pop_back();
             }
         }
+        
     }
     vector<vector<string>> partition(string s) {
-        vector<string >temp;
-        findans(0,s,temp);
+        vector<string>temp;
+         findans(s,0,temp);
         return ans;
     }
 };
