6e4dcca206175b9b77239b89709d7ab1b340353f
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-11-01 00:31:24 +0530

    Time: 7 ms (85.51%), Space: 6.7 MB (79.30%) - LeetHub

--- 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
     int dp[101][101];
-    int check(int i,int j,string &s)
-    {
-        int count=0;
+    int getval(string & s,int i,int j){
+        int count =0;
         while(i<=j)
         {
-            if(s[i]!=s[j])
+            if(s[i++]!=s[j--])
                 count++;
-            i++;
-            j--;
         }
         return count;
     }
-    int findans(string &s,int i,int k)
-    {
-        if(i>=s.size())
-            return 0;
+    int findans(string &s,int i,int k){
+        
         
         if(dp[i][k]!=-1)
             return dp[i][k];
-        int ans=INT_MAX;
+        int ans=1e5;
         if(k==1)
-            ans=check(i,s.size()-1,s)+findans(s,s.size(),0);
+            ans=getval(s,i,s.size()-1);
         else
-        for(int j=i;j<s.size()-(k-1);j++)
-        {
-            int count=check(i,j,s);
-            ans=min(ans,count+findans(s,j+1,k-1));
+        for(int j=i;j<=s.size()-k;j++){
+            ans=min(ans,getval(s,i,j)+findans(s,j+1,k-1));
         }
         return dp[i][k]=ans;
         
-    }
-    int palindromePartition(string s, int k) { 
-        memset(dp,-1,sizeof(dp));
         
+    }
+    int palindromePartition(string s, int k) {
+        memset(dp,-1,sizeof(dp));
         return findans(s,0,k);
     }
 };
