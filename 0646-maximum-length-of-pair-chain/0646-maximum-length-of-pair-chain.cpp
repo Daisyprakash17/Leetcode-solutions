a9184657635207b182d30dd2b4457875b9ca5e70
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-11-01 00:14:40 +0530

    Time: 51 ms (74.38%), Space: 22.8 MB (75.85%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    int dp[1000];
-    int findans(vector<vector<int>>&p,int i)
-    {
-        if(i==p.size())
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        
-        int ans=1;
-        for(int j=i+1;j<p.size();j++)
-        {
-            if(p[i][1]<p[j][0])
-                ans=max(ans,1+findans(p,j));
-        }
-        return dp[i]=ans;
+    static bool cmp(vector<int>&a,vector<int>&b){
+        if(a[0]==b[0])
+            return a[1]<b[1];
+        return a[0]<b[0];
     }
-    int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(),pairs.end());
-        memset(dp,-1,sizeof(dp));
-        int ans=0;
-        for(int i=0;i<pairs.size();i++)
-        {
-            ans=max(ans,findans(pairs,i));
-        }
-        return ans;
+    int findLongestChain(vector<vector<int>>& v) {
+        sort(v.begin(),v.end(),cmp);
+        int last=INT_MAX;
+        int count=0;
+            for(int i=v.size()-1;i>=0;i--){
+                if(v[i][1]<last){
+                    count++;
+                    last=v[i][0];
+                }
+            }
+        return count;
     }
+    
 };
