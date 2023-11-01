094a6df8b3652f56e390ce9e8075196f2bbf3cae
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-11-01 23:20:46 +0530

    Time: 104 ms (83.18%), Space: 13.2 MB (64.20%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     int dp[101][101][101];
-    int findans(int n,int cursum,int i,vector<int>&g,vector<int>&p)
-    {
-           
-        if(i>=g.size() )
-        {
-            if(cursum>0)
-                return 0;
-            else
-                return 1;
+    int mod=1e9+7;
+    int findans(int i,int n,int mp ,vector<int>&g,vector<int>&p){
+        if(i>=p.size())
+            return mp<=0;
+        if(mp<0)
+            mp=0;
+        if(dp[i][n][mp]!=-1)
+            return dp[i][n][mp];
+        
+        int ans=0;
+        if(g[i]<=n){
+            ans=(ans+findans(i+1,n-g[i],mp-p[i],g,p))%mod;
         }
-            
-        if(cursum<0)
-            cursum=0; 
-        if(dp[n][cursum][i]!=-1)
-            return dp[n][cursum][i];
-        int val=0;
-        int mod=1e9+7;
-        if(g[i]<=n)
-            val=(findans(n-g[i],cursum-p[i],i+1,g,p)+findans(n,cursum,i+1,g,p))%mod;
-        else
-            val=findans(n,cursum,i+1,g,p)%mod;
-        return dp[n][cursum][i]=val;
+        ans=(ans+findans(i+1,n,mp,g,p))%mod;
+        return dp[i][n][mp]=ans;
     }
-    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+    int profitableSchemes(int n, int mp, vector<int>& g, vector<int>& p) {
         memset(dp,-1,sizeof(dp));
-        
-        
-        return findans(n,minProfit,0,group,profit);
+        return findans(0,n,mp,g,p);
     }
+    
+    
 };
