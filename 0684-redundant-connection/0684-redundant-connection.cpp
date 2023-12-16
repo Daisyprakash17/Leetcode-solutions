381c516a79a2455567a436f2bf0373495b3b6151
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-12-16 12:27:52 +0530

    Time: 7 ms (50.89%), Space: 11.3 MB (13.11%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,54 +1,51 @@
 class Solution {
 public:
-    void findans(vector<vector<int>>&arr,int node,int& timer,vector<int>&low,vector<int>&desc,vector<pair<int,int>>&v,int par)
-    {
-     low[node]=desc[node]=timer++;
-        for(auto i:arr[node])
-        {
-                if(i==par)
-                    continue;
-            if(desc[i]==-1){
-            findans(arr,i,timer,low,desc,v,node);
-            low[node]=min(low[node],low[i]);
-            }else{
-                low[node]=min(low[node],desc[i]);
+    int last=-1;
+    vector<int>ans;
+        map<pair<int,int>,int>m;
+    void findans(vector<vector<int>>&arr,int node,vector<int>&low,vector<int>&disc,int timer,int par){
+        low[node]=disc[node]=timer;
+        timer++;
+        for(auto i:arr[node]){
+            if(i==par)
+                continue;
+            if(disc[i]==-1){
+                findans(arr,i,low,disc,timer,node);
+                low[node]=min(low[i],low[node]);
             }
-            if(low[node]==low[i])
-                v.push_back({node,i});
+            else{
+                low[node]=min(low[node],disc[i]);
+            }
+            if(low[node]==low[i]){
+                    // this is the cycle
+                    if(m[{node,i}]>=last){
+                        last=m[{node,i}];
+                        ans.clear();
+                        ans={node,i};
+                    }if(m[{i,node}]>=last){
+                        last=m[{i,node}];
+                        ans.clear();
+                        ans= {i,node};
+                    }
+                }
         }
     }
-    vector<int> findRedundantConnection(vector<vector<int>>& v) {
-        int n=v.size();
-        vector<vector<int>>arr(n+1);
-        map<pair<int,int>,int>m;
-        int in=1;
-        for(auto i:v)
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size()+1;
+        vector<vector<int>>arr(n);
+        int t=1;
+        for(auto i:edges)
         {
             arr[i[0]].push_back(i[1]);
             arr[i[1]].push_back(i[0]);
-            m[{i[0],i[1]}]=in++;
-        } 
-        vector<int>low(n+1,-1);
-        vector<int>desc(n+1,-1);
-        in=1;
-        vector<pair<int,int>>temp;
-        findans(arr,1,in,low,desc,temp,-1);
-        
-        int cur=-1;
-        vector<int>ans;
-        for(auto i:temp)
-        { 
-            if(m[{i.first,i.second}]>cur){
-                cur=m[{i.first,i.second}];
-                ans.clear();
-                ans.push_back(i.first);
-                ans.push_back(i.second);
-            }else if(m[{i.second,i.first}]>cur)
-            {
-                cur=m[{i.second,i.first}];
-                ans.clear();
-                ans.push_back(i.second);
-                ans.push_back(i.first);
+            m[{i[0],i[1]}]=t++;
+        }
+        vector<int>low(n,-1);
+        vector<int>disc(n,-1);
+        int time=1;
+        for(int i=0;i<n;i++){
+            if(disc[i]==-1){
+                findans(arr,i,low,disc,time,-1);
             }
         }
         return ans;
