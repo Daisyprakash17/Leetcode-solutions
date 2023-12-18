39ee2515dcd4e808b16e49c0bdfadfa9a4a44a21
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-12-18 22:26:33 +0530

    Time: 496 ms (17.65%), Space: 145.2 MB (28.50%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    void findans(int node,vector<pair<int,int>>arr[],vector<int>&vis,int &ans)
-    {
-    
-    vis[node]=1;
-        for(auto i:arr[node])
-        {
-            if(vis[i.first]==0)
-            {
-                findans(i.first,arr,vis,ans);
-            }
-            
-                ans=min(ans,i.second);
-        }
-    }
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<pair<int,int>>arr[n+1];
-        for(auto i:roads)
-        {
+        vector<vector<pair<int,int>>>arr(n+1);
+
+        for(auto i:roads){
             arr[i[0]].push_back({i[1],i[2]});
             arr[i[1]].push_back({i[0],i[2]});
         }
-        vector<int>vis(n+1,0);
-        int ans=INT_MAX;
-        findans(1,arr,vis,ans);
-        return ans;
+        
+        vector<int>dis(n+1,INT_MAX);
+        dis[1]=INT_MAX;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        q.push({INT_MAX,1});
+        while(q.size()>0){
+            int node=q.top().second;
+            int d=q.top().first;
+            q.pop();
+            for(auto it:arr[node]){
+                if(dis[it.first]>min(it.second,d)){
+                    dis[it.first]=min(it.second,d);
+                    q.push({dis[it.first],it.first});
+                }
+            }
+        }
+        return dis[n];
     }
 };
