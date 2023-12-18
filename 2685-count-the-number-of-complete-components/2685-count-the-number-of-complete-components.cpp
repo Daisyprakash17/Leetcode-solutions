47b6e8229cc1c6489a46afa8b2d3bfddb5c41eb3
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2023-12-18 22:47:44 +0530

    Time: 202 ms (38.42%), Space: 130.4 MB (37.98%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,56 +1,38 @@
 class Solution {
 public:
-    void findans(vector<vector<int>>&arr,int node,vector<int>&vis,vector<int>&dvis)
-    {
+    void fillval(vector<vector<int>>&arr,int node,vector<int>&vis,vector<int>&val,vector<int>&temp){
         vis[node]=1;
-        dvis[node]=1; 
-        for(auto i:arr[node])
-        {
-            if(vis[i]==0)
-                findans(arr,i,vis,dvis);
-        }
-    }
-    int check(vector<int>&v,map<pair<int,int>,int>&m)
-    { 
-        int n=v.size();
-        int flag=1;
-        for(int i=0;i<n;i++)
-        {
-            if(v[i]==0)
+        temp.push_back(node);
+        for(auto i:arr[node]){
+            val[i]++;
+            if(vis[i]==1)
                 continue;
-            for(int j=i+1;j<n;j++)
-                if(v[j]==1)
-                { 
-                 if(!((m[{i,j}]==1 || m[{j,i}]==1)))
-                     flag=0;
-                } 
-            v[i]=0;
+            fillval(arr,i,vis,val,temp);
         }
-      
-        return flag;
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
         vector<vector<int>>arr(n);
-        map<pair<int,int>,int>m;
-        for(auto i:edges)
-        {
+        for(auto i:edges){
             arr[i[0]].push_back(i[1]);
             arr[i[1]].push_back(i[0]);
-            m[{i[0],i[1]}]=1;
         }
-        
-        vector<int>vis(n,0),dvis(n,0);
+        vector<int>val(n,0);
+        vector<int>vis(n,0);
         int count=0;
-        for(int i=0;i<n;i++)
-        {
-            if(vis[i]==0)
-            {
-                findans(arr,i,vis,dvis);
-                if(check(dvis,m)==true)
+        vector<int>temp;
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+            fillval(arr,i,vis,val,temp);
+                int flag=1;
+                for(auto i:temp){
+                    if(val[i]!=temp.size()-1)
+                        flag=0;    
+                }
+                if(flag==1)
                     count++;
+                temp.clear();
             }
         }
         return count;
-        
     }
 };
