25ab147b63559be61846e376aae23f1e27551330
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2024-01-22 22:17:38 +0530

    Time: 275 ms (98.55%), Space: 146.2 MB (7.37%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,37 +1,37 @@
 class NumMatrix {
 public:
-    vector<vector<int>>v;
-    NumMatrix(vector<vector<int>>& t) {
+    vector<vector<int>>temp;
+    NumMatrix(vector<vector<int>>& v) {
         
-        int n=t.size();
-        int m=t[0].size();
-        for(int i=0;i<n;i++)
-        {
-            vector<int>temp;
-            temp.push_back(t[i][0]);
-            for(int j=1;j<m;j++)
-                temp.push_back(temp[j-1]+t[i][j]);
-            
-            v.push_back(temp);
+        int n=v.size();
+        int m=v[0].size();
+        
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=1;j<m;j++){
+                    v[i][j]+=v[i][j-1];
+                }
         }
-         
-        for(int j=0;j<m;j++)
-        {
-            for(int i=1;i<n;i++)
+        
+        for(int j=0;j<m;j++){
+            for(int i=1;i<n;i++){
                 v[i][j]+=v[i-1][j];
+            }
         }
+        temp=v;
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-         
-        int sum=v[row2][col2];
-        if(row1-1>=0)
-            sum-=v[row1-1][col2];
-        if(col1-1>=0)
-            sum-=v[row2][col1-1];
-        if(row1-1>=0 && col1-1>=0)
-            sum+=v[row1-1][col1-1];
-        return sum;
+        
+        int ans=0;
+        ans+=temp[row2][col2];
+        if((row1-1)>=0)
+            ans-=temp[row1-1][col2];
+        if((col1-1)>=0)
+            ans-=temp[row2][col1-1];
+        if((row1-1)>=0 && (col1-1)>=0)
+            ans+=temp[row1-1][col1-1];
+        return ans;
     }
 };
 
