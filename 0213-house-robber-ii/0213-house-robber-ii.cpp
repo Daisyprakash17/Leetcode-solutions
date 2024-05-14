4545fe1df28385e433138c79e996b774e57ccfeb
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2024-05-14 22:15:57 +0530

    Time: 4 ms (32.76%), Space: 9.1 MB (82.46%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int dp[101];
-    int findans(vector<int>&nums,int i,int last)
-    {
-        if(i<=last)
-            return 0;
-        if(dp[i]!=-1)
-            return  dp[i]; 
-        return dp[i]=max(nums[i]+findans(nums,i-2,last),findans(nums,i-1,last));
-    }
-    int rob(vector<int>& nums) {
-        if(nums.size()==1)
-            return nums[0];
+    int dp[1000][2];
+        int findans(vector<int>&v,int i,int t){
+            if((t==0 && i>=v.size()-1) || (t==1 && i>=v.size()))
+                return 0;
+            
+            if(dp[i][t]!=-1)
+                return dp[i][t];
+            
+            int ans=max(v[i]+findans(v,i+2,t),findans(v,i+1,t));
+            return dp[i][t]=ans;
+        }
+    int rob(vector<int>& v) {
         memset(dp,-1,sizeof(dp));
-        int val=findans(nums,nums.size()-1,0);
-        memset(dp,-1,sizeof(dp));
-        val=max(val,findans(nums,nums.size()-2,-1));
-        return val;
+        if(v.size()==1)
+            return v[0];
+        return max(findans(v,0,0),findans(v,1,1));
     }
 };
