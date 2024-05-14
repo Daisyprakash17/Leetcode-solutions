4277d5119df4fb37c44cde19d2dbd5354b1717da
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2024-05-14 07:30:28 +0530

    Time: 257 ms (21.20%), Space: 74.8 MB (8.23%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,51 +1,52 @@
 class Solution {
 public:
-    
-    int findans(vector<int>arr[],vector<int>&vis,int node)
-    {
-            vis[node]=1;
-        int count=1;
-        for(auto i:arr[node])
-        {
-            if(vis[i]==0)
-            {
-            count+=findans(arr,vis,i);
-            }
-        }
-        return count;
+
+    int isOverlap(vector<int> bomb1, vector<int> bomb2){
+        long long int x1 = bomb1[0];
+        long long int y1 = bomb1[1];
+        long long int r1 = bomb1[2];
+
+        long long int x2 = bomb2[0];
+        long long int y2 = bomb2[1];
+        long long int r2 = bomb2[2];
+
+        if((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) <= r1*r1)
+            return true;
+        return false;
     }
-    int maximumDetonation(vector<vector<int>>& b) {
-        
-        
-        int n=b.size();
-        vector<int>arr[n];
-        for(int i=0;i<n;i++)
-        {
-        for(int j=0;j<n;j++)
-        {
-            if(i==j)
-                continue;
-            long long  first=b[i][0]-b[j][0];
-            long long second=b[i][1]-b[j][1]; 
-           int rad=(b[i][2] );
-            if(sqrt(first*first+second*second)<=(rad ))
-            {
-                arr[i].push_back(j); 
+
+    int dfs(int node, vector<int>& isVisited, vector<vector<int>>& graph){
+        isVisited[node] = 1;
+
+        int sum = 0;
+        for(int i = 0; i < graph[node].size(); i++){
+            int next = graph[node][i];
+            if(!isVisited[next])
+                sum += dfs(next, isVisited, graph);
+        }
+        return 1 + sum;
+    }
+
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int N = bombs.size();
+        vector<vector<int>> graph(N);
+
+        for(int i = 0; i < N; i++){
+            for(int j = i+1; j < N; j++){
+                if(isOverlap(bombs[i], bombs[j])){
+                    graph[i].push_back(j);
+                }
+                if(isOverlap(bombs[j], bombs[i])){
+                    graph[j].push_back(i);
+                }
             }
         }
+
+        int result = 0;
+        for(int i = 0; i < N; i++){
+            vector<int> isVisited(N, 0);
+            result = max(result, dfs(i, isVisited, graph));
         }
-        
-            
-            int ans=INT_MIN;
-            for(int i=0;i<n;i++)
-            {
-                 
-                    
-                    vector<int>vis(n,0);
-                        ans=max(ans,findans(arr,vis,i));
-                 
-            }
-            return ans;
-        
+        return result;
     }
 };
