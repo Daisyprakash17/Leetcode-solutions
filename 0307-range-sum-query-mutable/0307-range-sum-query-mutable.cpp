6fcd299a88bba8d2e7ef0c2f460a71e65eb9ca4c
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2024-05-24 13:56:52 +0530

    Time: 350 ms (86.85%), Space: 178.9 MB (29.66%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,63 +1,61 @@
-vector<int>a,tree;
-void build(int node,int st,int en)
-{
-
-    if(st==en)
-    {
-
-        tree[node]=a[st];
-        return ;
-    }
-    int mid=(st+en)/2;
-    build(2*node,st,mid);
-    build(2*node+1,mid+1,en);
-    tree[node]=tree[2*node]+tree[2*node+1];
-}
-int query(int node,int st,int en,int l,int r)
-{
-
-    if(st>r || en<l)
-        return 0; // if no overlapping
-    if(st>=l && en<=r)
-        return tree[node]; // if there is complete over lapping
-
-
-    // but if here is partial over lapping that means that we will
-    // so divide and conquer and call for each of the segment
-    int mid=(st+en)/2;
-    int left=query(2*node,st,mid,l,r);
-    int right=query(2*node+1,mid+1,en,l,r);
-    return left+right;
-}
-void updateNode(int node,int index,int val,int st,int en)
-{
-    if(st==index && en==index)
-    {
-        tree[node]=val;
-        return ;   
-    }
-    int mid=(st+en)/2;
-    if(index>=st && index<=mid)
-        updateNode(2*node,index,val,st,mid);
-    if(index>=mid+1 && index<=en)
-        updateNode(2*node+1,index,val,mid+1,en);
-    tree[node]=tree[2*node]+tree[2*node+1];
-}
 class NumArray {
 public:
-    NumArray(vector<int>& nums) {
-        a.resize(nums.size(),0);
-        tree.resize(4*nums.size()+4,0);
-        a=nums;
-        build(1,0,nums.size()-1);
+    vector<int>nodes;
+    vector<int>nums;
+    void createtree(int node,int l,int r){
+        
+        if(l==r){
+            nodes[node]=nums[l];
+            return ;
+        }
+        int mid=(l+r)/2;;
+        createtree(2*node,l,mid);
+        createtree(2*node+1,mid+1,r);
+        int left=nodes[2*node];
+        int right=nodes[2*node+1];
+        nodes[node]=left+right;
+    }
+    void updatevalue(int node,int index,int val,int l,int r){
+        
+        if(l==r && l==index){
+            nodes[node]=val;
+            return ;
+        }
+        int mid=(l+r)/2;
+        if(index>=l &&index<=mid){
+            // so this value is in left parts so
+            updatevalue(2*node,index,val,l,mid);
+        }
+        else if(index>=mid+1 &&index<=r)
+            updatevalue(2*node+1,index,val,mid+1,r);
+        nodes[node]=nodes[2*node]+nodes[2*node+1];
+    }
+    int getsum(int node,int l,int r,int left,int right){
+        
+        if(r<left || right<l)
+            return 0;
+        if(left<=l && right>=r)
+            return nodes[node];
+        
+        int mid=(l+r)/2;
+        int ll=getsum(2*node,l,mid,left,right);
+         int rr=  getsum(2*node+1,mid+1,r,left,right);
+        return ll+rr;
+        
+    }
+    NumArray(vector<int>& num) {
+        nodes.resize(4*num.size()+5,0);
+        nums=num;
+        createtree(1,0,num.size()-1);
     }
     
     void update(int index, int val) {
-        updateNode(1,index,val,0,a.size()-1);
+        updatevalue(1,index,val,0,nums.size()-1);
     }
     
     int sumRange(int left, int right) {
-        return query(1,0,a.size()-1,left,right);
+        return getsum(1,0,nums.size()-1,left,right);
+        return 0;
     }
 };
 
