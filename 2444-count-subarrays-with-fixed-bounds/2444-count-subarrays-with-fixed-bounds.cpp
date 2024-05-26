f989817e177443066a0cccfe526a69a8d3c4b3ee
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2024-05-26 13:24:51 +0530

    Time: 101 ms (11.03%), Space: 82.5 MB (85.47%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    long long countSubarrays(vector<int>& v, int minK, int maxK) {
-        long long ans=0;
+    long long countSubarrays(vector<int>& nums, int mi, int ma) {
+        int n=nums.size();
+        int last=n;
         
-        vector<int>mi,ma;
-        vector<int>right;
-        for(int i=0;i<v.size();i++)
-        {
-                if(v[i]==minK)
-                    mi.push_back(i);
-                  if(v[i]==maxK)
-                    ma.push_back(i);
-                  if(v[i]<minK  || v[i]>maxK)
-                    right.push_back(i);
+        int mini=INT_MAX; // least index of mi value
+        int maxi=INT_MAX; // least index of ma value
+        long long sum=0;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>ma || nums[i]<mi){
+                last=i;
+                mini=INT_MAX;
+                maxi=INT_MAX;
+            }
+            else{
+                if(nums[i]==mi)
+                    mini=i;
+                 if(nums[i]==ma)
+                    maxi=i;
+                
+                if(mini==INT_MAX || maxi==INT_MAX)
+                    continue;
+                
+                int maazi=max(mini,maxi);
+                sum+=last-maazi;
+                
+            }
         }
-        
-        right.push_back(v.size());
-        for(int i=0;i<v.size();i++)
-        {
-            int mini=-1;
-            int maxi=-1;
-            int ri=-1;
-            auto it=lower_bound(mi.begin(),mi.end(),i);
-            if(it!=mi.end())
-                mini=*it;
-            it=lower_bound(ma.begin(),ma.end(),i);
-            if(it!=ma.end())
-                maxi=*it ;
-            it=lower_bound(right.begin(),right.end(),i) ;
-            ri=*it;
-            if(maxi==-1 || mini==-1)
-                break;
-            if(ri>mini && ri>maxi)
-                ans+=ri-max(mini,maxi);
-        }
-        return ans ;
+        return sum;
     }
 };
