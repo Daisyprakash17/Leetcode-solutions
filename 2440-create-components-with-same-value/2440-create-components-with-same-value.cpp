3bd7b04976b3fa774f7a0a92356ccc7d312e45be
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2024-05-26 13:06:55 +0530

    Time: 424 ms (30.34%), Space: 160.4 MB (12.36%) - LeetHub

--- 2440-create-components-with-same-value/2440-create-components-with-same-value.cpp
+++ 2440-create-components-with-same-value/2440-create-components-with-same-value.cpp
@@ -1,59 +1,53 @@
 class Solution {
 public:
-    int findans(vector<vector<int>>&v,vector<int>&vis,int& count,int node,int sum ,vector<int>&nums)
-    {
+    int getans(vector<vector<int>>&arr,int node,vector<int>&nums,bool & flag,int sum,vector<int>&vis){
+        
+        
         vis[node]=1;
-        int cursum=nums[node];
-        for(auto i:v[node])
-        {
-            if(vis[i]==1)
-                continue;
-            int temp=findans(v,vis,count,i,sum,nums);
-            if(temp!=-1)
-                cursum+=temp;
-            else{
-                vis[node]=0;
-                return -1;
+        int tempsum=nums[node];
+        for(auto i:arr[node]){
+            if(vis[i]==0){
+                
+               tempsum+=getans(arr,i,nums,flag,sum,vis);
             }
         }
-        if(cursum==sum)
-        {
-            count++;
-            cursum=0;
-        }
-        else if(cursum>sum)
-            cursum=-1;
-        vis[node]=0;
-        return cursum;
+        if(tempsum==sum)
+            tempsum=0;
+        
+        if(tempsum>sum)
+            flag=false;
+        return tempsum;
     }
-    int componentValue(vector<int>& nums, vector<vector<int>>& edges) {
+    int findans(vector<vector<int>>&arr,int n,vector<int>&nums){
+                
         int sum=0;
-        int n=nums.size();
         for(auto i:nums)
             sum+=i;
-        vector<vector<int>>v(n);
-        for(auto i:edges)
-        {
-            v[i[0]].push_back(i[1]);
-            v[i[1]].push_back(i[0]);
+       int ans=0;
+        cout<<sum<<endl;
+        for(int i=2;i<=sum;i++){
+            if(sum%i==0){
+                cout<<"calling for "<<i<<endl;
+                vector<int>vis(n,0);
+                bool flag=true;
+                cout<<"about to call for "<<i<<endl;
+                getans(arr,0,nums,flag,sum/i,vis);
+                cout<<flag<<endl;
+                if(flag)
+                    ans=max(ans,i-1);
+            }
         }
-        vector<int>vis(n,0);
-        int ans=0;
-        int count=0;
-        findans(v,vis,count,0,6,nums);
-         for(int i=1;i<=sqrt(sum);i++)
-        {
-            if(sum%i==0)
-            { 
-                 count=0;  
-                if(findans(v,vis,count,0,i,nums)==0)
-                    ans=max(ans,count-1);
-                 count=0;
-                if(findans(v,vis,count,0,sum/i,nums)==0)
-                    ans=max(ans,count-1);
-             }
-        }
+        cout<<"the answer is "<<ans<<endl;
         return ans;
+    }
+    int componentValue(vector<int>& nums, vector<vector<int>>& edges) {
         
+        int n=nums.size();
+        vector<vector<int>>arr(n);
+        for(auto i:edges){
+            arr[i[0]].push_back(i[1]);
+            arr[i[1]].push_back(i[0]);
+        }
+        return findans(arr,n,nums);
     }
 };
