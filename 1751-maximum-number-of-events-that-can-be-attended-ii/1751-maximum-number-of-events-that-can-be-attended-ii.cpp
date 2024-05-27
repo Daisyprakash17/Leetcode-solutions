0aa4da4d987cee026fa3c5f52c5fe847914a126e
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2024-05-27 21:31:33 +0530

    Time: 321 ms (80.20%), Space: 124 MB (23.15%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    map<int,vector<pair<int,int>>>m;
-    set<int>s; // this is the store the unique starting indexes
-    map<pair<int,int>,int>dp; // this is to store the res of sub problem
-   
-    int findans(int i,int k)
-    {
-        if(dp.find({i,k})!=dp.end())
-            return dp[{i,k}];
+    int solve(vector<vector<int>>&dp, vector<vector<int>>& events, int n, int pos, int k)
+{
+        int i;
         
-        auto it=s.lower_bound(i);
-        int temp=0;
-        while(it!=s.end() && k>0)
-        {
-            auto st=(*it);
-            it++;
-            
-            for(auto j:m[st])
-            {
-                temp=max(temp,j.second+findans(j.first+1,k-1));
-            }
-        }
-        return dp[{i,k}]=temp;
+        if(pos >= n || k == 0)
+            return 0;
+        
+        if(dp[pos][k] != -1)
+            return dp[pos][k];
+        
+        // finding the next event which we can attend
+        for(i = pos + 1; i < n; i++)
+            if(events[i][0] > events[pos][1])
+                break;
+        
+       return dp[pos][k] = max(solve(dp, events, n, pos+1, k), events[pos][2] + solve(dp, events, n, i, k-1));
+
     }
     
-    int maxValue(vector<vector<int>>& events, int k) {
-        for(auto i:events){
-            m[i[0]].push_back({i[1],i[2]});
-            s.insert(i[0]);        
-    }
-        return findans(1,k);
+    int maxValue(vector<vector<int>>& events, int k) 
+    {
+        int n = events.size();
+        sort(events.begin(), events.end());
+        
+        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+        
+        return solve(dp, events, n, 0, k);
     }
 };
