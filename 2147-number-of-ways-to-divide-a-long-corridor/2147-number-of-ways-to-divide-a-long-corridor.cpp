264486bfc228bcaf623dccc468aed70729bb184f
Daisy prakash khoja <70110042+Daisyprakash17@users.noreply.github.com>
2024-07-19 12:17:09 +0530

    Time: 138 ms (38.65%), Space: 57.2 MB (25.77%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    vector<int>pos;
-    int dp[100000];
-    int mod=1e9+7;
-    int findans(string &s,int i,int n)
-    {
-        if(i>=n-1)
-            return 1;
-        if(dp[i]!=-1)
-            return dp[i];
-        auto first=lower_bound(pos.begin(),pos.end(),i);
-         
-        auto second=lower_bound(pos.begin(),pos.end(),(*first)+1);
-         if(pos[pos.size()-1]==*second)
-             return dp[i]=1;
+    int numberOfWays(string corridor) {
+        vector<int>seatPos;
+        for(int i=0;i<corridor.size();i++){
+            if(corridor[i]=='S')
+                seatPos.push_back(i);
+        }
         
-        auto third=lower_bound(pos.begin(),pos.end(),(*second)+1);
-        long long diff=(*third)-(*second);
-         long long val=(diff*findans(s,*third,n))%mod;
-        return dp[i]=val;
-    }
-    int numberOfWays(string s) {
-            for(int i=0;i<s.size();i++)
-            {
-                if(s[i]=='S')
-                    pos.push_back(i);
-            }
-        if(pos.size()%2==1  || pos.size()==0)
+        if(seatPos.size()%2==1 || seatPos.size()==0)
             return 0;
         
-      memset(dp,-1,sizeof(dp));
-        
-        return findans(s,0,s.size());
+        long long  noOfPar=1;
+        long long  mod=1e9+7;
+        for(int i=0;i<seatPos.size();i+=2){
+            
+            if(i+2<seatPos.size()){
+                long long diff=seatPos[i+2]-seatPos[i+1];
+            noOfPar=(noOfPar*diff)%mod;
+            }
+        }
+        return noOfPar;
     }
 };
